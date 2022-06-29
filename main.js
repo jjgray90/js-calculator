d4e1db207f97afb61bd09512f7dc46109db283ca
Your Name <jjgray90@gmail.com>
2022-06-29 17:29:19 +0100

    created basic calculator functions

--- main.js
+++ main.js
@@ -1,25 +1,60 @@
-let inputArray = [];
+let inputArrayOne = [];
+let inputArrayTwo = [];
 let numOne = 0;
-let numTwo = 2;
+let numTwo = 0;
+let numThree = 0;
+let symbol;
 
 const getInput = (id) => {
   let input = document.getElementById(id).value;
 
   if (input === "clear") {
-    inputArray = [];
+    inputArrayOne = [];
+    inputArrayTwo = [];
+    numOne = 0;
+    numTwo = 0;
+    numThree = 0;
+    symbol = undefined;
+    document.getElementById("output").innerHTML = numOne;
+  } else if (!symbol) {
+    inputArrayOne.push(input);
+    numOne = parseInt(inputArrayOne.join(""));
+    document.getElementById("output").innerHTML = numOne;
   } else {
-    inputArray.push(input);
+    inputArrayTwo.push(input);
+    numTwo = parseInt(inputArrayTwo.join(""));
+    document.getElementById("output").innerHTML = numTwo;
   }
-  document.getElementById("output").innerHTML = inputArray.join("");
-  console.log(inputArray);
+  // console.log(numOne);
+  // console.log(numTwo);
+  // console.log(numThree);
+  // console.log(symbol);
 };
 
-const getEquation = (id) => {
-  numOne = parseInt(inputArray.join(""));
-  let method = document.getElementById(id).value;
+const getMethod = (id) => {
+  symbol = document.getElementById(id).value;
+};
 
-  if (method === "*") {
-    let result = numOne * numTwo;
-    console.log(result);
+const getEquation = () => {
+  switch (symbol) {
+    case "*":
+      numThree = numOne * numTwo;
+      break;
+
+    case "/":
+      numThree = numOne / numTwo;
+      break;
+
+    case "+":
+      numThree = numOne + numTwo;
+      break;
+
+    case "-":
+      numThree = numOne - numTwo;
+      break;
   }
+
+
+  numTwo = 0;
+  document.getElementById("output").innerHTML = numThree;
 };
