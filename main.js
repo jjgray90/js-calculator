d5ee9bb1b00689abeb3058f1bc46e55861a1e23e
Your Name <jjgray90@gmail.com>
2022-06-30 16:42:05 +0100

    working calc, takes more complex equations

--- main.js
+++ main.js
@@ -2,18 +2,25 @@ let inputArrayOne = [];
 let inputArrayTwo = [];
 let numOne = 0;
 let numTwo = 0;
-let numThree = 0;
 let symbol;
+
+let output = document.getElementById("output");
 
 const getReset = () => {
   inputArrayOne = [];
   inputArrayTwo = [];
   numOne = 0;
   numTwo = 0;
-  numThree = 0;
   symbol = undefined;
   document.getElementById("output").innerHTML = numOne;
 };
+
+// const inputToNumber = (array, bloop, input) => {
+//   array.push(input);
+//   bloop = parseInt(array.join(""));
+//   output.innerHTML = bloop;
+//   console.log(inputArrayOne);
+// };
 
 const getInput = (id) => {
   let input = document.getElementById(id).value;
@@ -21,22 +28,21 @@ const getInput = (id) => {
   if (input === "clear") {
     getReset();
   } else if (!symbol) {
+    // inputToNumber(inputArrayOne, numOne, input);
     inputArrayOne.push(input);
-    numOne = parseInt(inputArrayOne.join(""));
-    document.getElementById("output").innerHTML = numOne;
+    numOne = parseFloat(inputArrayOne.join(""));
+    output.innerHTML = numOne;
   } else {
     inputArrayTwo.push(input);
-    numTwo = parseInt(inputArrayTwo.join(""));
-    document.getElementById("output").innerHTML = numTwo;
+    numTwo = parseFloat(inputArrayTwo.join(""));
+    output.innerHTML = numTwo;
   }
+
+  console.log(inputArrayOne, inputArrayTwo, numOne, numTwo, symbol);
 };
 
-// const inputToNumber = (array, num) => {
-//   num = parseInt(array.join(""));
-//   return (document.getElementById("output").innerHTML = num);
-// };
-
 const getMethod = (id) => {
+  getEquation();
   symbol = document.getElementById(id).value;
   console.log(symbol);
 };
@@ -44,22 +50,28 @@ const getMethod = (id) => {
 const getEquation = () => {
   switch (symbol) {
     case "*":
-      numThree = numOne * numTwo;
+      numOne = numOne * numTwo;
+      inputArrayTwo = [];
+      numTwo = 0;
       break;
 
     case "/":
-      numThree = numOne / numTwo;
+      numOne = numOne / numTwo;
+      inputArrayTwo = [];
+      numTwo = 0;
       break;
 
     case "+":
-      numThree = numOne + numTwo;
+      numOne = numOne + numTwo;
+      inputArrayTwo = [];
+      numTwo = 0;
       break;
 
     case "-":
-      numThree = numOne - numTwo;
+      numOne = numOne - numTwo;
+      inputArrayTwo = [];
+      numTwo = 0;
       break;
   }
-
-  numTwo = 0;
-  document.getElementById("output").innerHTML = numThree;
+  output.innerHTML = numOne;
 };
